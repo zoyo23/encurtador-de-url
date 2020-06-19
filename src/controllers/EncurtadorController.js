836f5ec1c9c327c66f4c1126ca42a5c51b683bcf
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-06-19 06:25:32 -0300

    alteração para html dinâmico

--- src/controllers/EncurtadorController.js
+++ src/controllers/EncurtadorController.js
@@ -1,7 +1,17 @@
 module.exports = {
     index(req, resp) {
         return resp.marko(
-            require('../Views/Encurtador/Listagem/listagem.marko')
+            require('../Views/Encurtador/Listagem/listagem.marko'),
+            {
+                itens: listaItens
+            }
         );
     }
 };
+
+let listaItens = [
+    { id: 0, shortUrl: "https://resumeurl.herokuapp.com/teste01", destinyUrl: "https://google.com.br", clickCounter: 55, createDate: new Date(Date.now()).toLocaleString("pt-BR")},
+    { id: 1, shortUrl: "https://resumeurl.herokuapp.com/teste02", destinyUrl: "https://google.com.br", clickCounter: 55, createDate: new Date(Date.now()).toLocaleString("pt-BR")},
+    { id: 2, shortUrl: "https://resumeurl.herokuapp.com/teste03", destinyUrl: "https://google.com.br", clickCounter: 55, createDate: new Date(Date.now()).toLocaleString("pt-BR")},
+    { id: 3, shortUrl: "https://resumeurl.herokuapp.com/teste04", destinyUrl: "https://google.com.br", clickCounter: 55, createDate: new Date(Date.now()).toLocaleString("pt-BR")},
+];
